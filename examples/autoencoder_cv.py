5377a737c484348568b09a07d5339071627d8c11
Allen Tang <allentang.me@gmail.com>
2016-11-05 10:24:24 -0700

    delete dropout in conv example

--- examples/autoencoder_cv.py
+++ examples/autoencoder_cv.py
@@ -32,17 +32,18 @@ model_architecture_cv = [
         'layer': 'fullyconnected',
         'layer_size': 10,
         'activation': tf.nn.elu,
-        'dropout': 1.0,
+        'dropout': 0.9,
     },
 ]
 
-v = AutoEncoder(input_size=[28, 28, 1],
-                architecture=model_architecture_cv,
-                batch_size=100,
-                learning_rate=1e-5,
-                l2_reg=1e-5,
-                sesh=None,
-                name='cv_mnist_autoencoder',
-                )
+v = AutoEncoder(
+    input_size=[28, 28, 1],
+    architecture=model_architecture_cv,
+    batch_size=100,
+    learning_rate=1e-5,
+    l2_reg=1e-5,
+    sesh=None,
+    name='cv_mnist_autoencoder',
+)
 
 v.train(mnist, max_iter=2 ** 15, max_epochs=np.inf, verbose=True, saver=True)
