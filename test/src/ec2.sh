1273706763093e5dc96820642ab2e069fa5e7052
Allen Tang <allentang.me@gmail.com>
2016-12-07 17:14:46 -0800

    update ec2 script

--- test/src/ec2.sh
+++ test/src/ec2.sh
@@ -9,4 +9,6 @@ sudo pip install prettytensor
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.0rc0-cp27-none-linux_x86_64.whl
 sudo pip install --upgrade $TF_BINARY_URL
 
+mkdir ~/.config
+mkdir ~/.config/matplotlib
 echo "backend : Agg" >> ~/.config/matplotlib/matplotlibrc
